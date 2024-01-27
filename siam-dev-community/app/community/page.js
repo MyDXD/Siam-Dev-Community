6586915e794a550d6564accd3c19ff72367d43b7
MyDXD <dislove107@gmail.com>
2024-01-27 17:28:24 +0700

    หน้า user

---
+++ siam-dev-community/app/community/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Community() {
+  return (
+    <div class="flex mx-72 my-10">
+      <div class="flex-auto w-2/3 border-4 border-purple-400 p-20 m-1 bg-slate-200">
+        test
+      </div>
+      <div class="flex-auto w-1/3 border-4 border-purple-400 p-20  m-1">
+        
+      </div>
+    </div>
+  )
+}
